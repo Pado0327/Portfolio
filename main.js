2e9ab5452e9dc58250991150728a940e575f9d4b
Jaejin(Ryan) Kim <pado0327@gmail.com>
2020-11-04 23:54:53 -0600

    add typing effect and btn animation

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 'use strict';
 
+// Home
+// nav animation 
 let home = document.querySelector('#home'); 
 let nav = document.querySelector('.header__nav');
 let navBtn = document.querySelector('.mobile__nav__menu');
 
-// nav animation
+
 window.onscroll = () =>{
     NavPop();
 }
 
+// nav hamburger btn event
 navBtn.addEventListener('click', ()=>{
     nav.classList.toggle('down');
+    navBtn.classList.toggle('change');
 })
 
+// popup nav after home section. 
 function NavPop() {
     if(window.pageYOffset > home.clientHeight){
         if(!nav.classList.contains('header__navMoved'))
@@ -23,3 +28,23 @@ function NavPop() {
         nav.classList.remove('header__navMoved');
     }
 }
+
+// home description typing effect 
+let description = document.querySelector("#textContainer");
+let text = description.innerText; 
+
+
+//TODO: may need to add more texts later. 
+function printDescription() {
+    description.innerHTML = '';
+    let index = 0;
+    let timer = setInterval(function () {
+      description.innerHTML += text.charAt(index);
+      if (++index == description.length) {
+        clearInterval(timer);
+      }
+    }, 100);
+  }
+  
+
+window.onload = () => printDescription();
