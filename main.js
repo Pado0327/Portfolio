ab982b2a38b1914a5e1e1dd377637aa47c0f2982
Jaejin(Ryan) Kim <pado0327@gmail.com>
2021-01-14 00:14:40 -0600

    Added scroll to the section function

--- main.js
+++ main.js
@@ -1,96 +1,117 @@
 'use strict';
 
 // Home
-// nav animation 
-const home = document.querySelector('#home'); 
+// nav animation
+const home = document.querySelector('#home');
 const nav = document.querySelector('.header__nav');
 const navBtn = document.querySelector('.mobile__nav__menu');
-const navList = document.querySelector('.nav_menulists');
 const arrowUpBtn = document.querySelector('.topbtn');
 
-// popup nav after home section. 
+// popup nav after home section.
 function NavAndArrowUpPop() {
-  if(window.pageYOffset >= home.clientHeight){
-      if(!nav.classList.contains('header__navMoved')){
-        nav.classList.add('header__navMoved');
-      }
-      arrowUpBtn.classList.add('visibleArrow');
-  } 
-  else if (window.pageYOffset < home.clientHeight){
-      if(nav.classList.contains('header__navMoved')){
-        nav.classList.remove('header__navMoved');
-      } 
-      arrowUpBtn.classList.remove('visibleArrow');
+  if (window.pageYOffset >= home.clientHeight) {
+    if (!nav.classList.contains('header__navMoved')) {
+      nav.classList.add('header__navMoved');
+    }
+    arrowUpBtn.classList.add('visibleArrow');
+  } else if (window.pageYOffset < home.clientHeight) {
+    if (nav.classList.contains('header__navMoved')) {
+      nav.classList.remove('header__navMoved');
+    }
+    arrowUpBtn.classList.remove('visibleArrow');
   }
 }
 
 // nav hamburger btn event
-navBtn.addEventListener('click', ()=>{
+navBtn.addEventListener('click', () => {
   nav.classList.toggle('down');
   navBtn.classList.toggle('change');
-})
-
-// scroll animation. 
-document.addEventListener('scroll', () =>{
-  NavAndArrowUpPop();
-  nav.classList.remove('down');
-  navBtn.classList.remove('change'); 
 });
 
+//scrolling to the section
+const navList = document.querySelector('.nav__menulists');
+const contactBtn = document.querySelector('.contactmebtn');
 
-// home description typing effect 
-const description = document.querySelector("#textContainer");
-let text = description.innerText; 
-
-//TODO: may need to add more texts later. 
-function printDescription() {
-    description.innerHTML = '';
-    let index = 0;
-    let timer = setInterval(function () {
-      description.innerHTML += text.charAt(index);
-      if (++index == description.length) {
-        clearInterval(timer);
-      }
-    }, 100);
+function OnClick(e) {
+  const target = e.target;
+  const dataLink = target.dataset.link;
+  if (dataLink == null) {
+    return;
   }
+  document.querySelector(`${dataLink}`).scrollIntoView({ behavior: 'smooth' });
+}
+
+navList.addEventListener('click', (e) => {
+  OnClick(e);
+});
+
+contactBtn.addEventListener('click', (e) => {
+  OnClick(e);
+});
+
+// scroll animation.
+document.addEventListener('scroll', () => {
+  NavAndArrowUpPop();
+  nav.classList.remove('down');
+  navBtn.classList.remove('change');
+});
+
+// home description typing effect
+const description = document.querySelector('#textContainer');
+let text = description.innerText;
+
+function printDescription() {
+  description.innerHTML = '';
+  let index = 0;
+  let timer = setInterval(function () {
+    description.innerHTML += text.charAt(index);
+    if (++index == description.length) {
+      clearInterval(timer);
+    }
+  }, 100);
+}
 
 window.onload = () => printDescription();
 
-// header contents faing effect when scorll down. 
+// header contents fading effect when scorll down.
 const headerMain = document.querySelector('.main__container');
 
 document.addEventListener('scroll', () => {
-  if(window.scrollY <= home.clientHeight){
-    headerMain.style.opacity = (home.clientHeight - window.scrollY)/home.clientHeight;
+  if (window.scrollY <= home.clientHeight) {
+    headerMain.style.opacity =
+      (home.clientHeight - window.scrollY) / home.clientHeight;
   }
-})
+});
 
 // project
 const projectbtnContainer = document.querySelector('.project__categories');
 const projects = document.querySelectorAll('.project__item');
-const projectContainer = document.querySelector('.projects__projectsWithDescription');
+const projectContainer = document.querySelector(
+  '.projects__projectsWithDescription'
+);
 const projectbtn = document.querySelectorAll('.project__btn');
 
 // show corresponding project items with animation.
-projectbtnContainer.addEventListener('click', event => {
-  const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
-  if(filter == null) {
+projectbtnContainer.addEventListener('click', (event) => {
+  const filter =
+    event.target.dataset.filter || event.target.parentNode.dataset.filter;
+  if (filter == null) {
     return;
   }
 
-  projectbtn.forEach( btn => {
-    if(filter == btn.dataset.filter){
+  projectbtn.forEach((btn) => {
+    if (filter == btn.dataset.filter) {
       btn.classList.add('active');
     } else {
       btn.classList.remove('active');
     }
   });
-  
+
   projectContainer.classList.add('anim-out');
 
   setTimeout(() => {
-    projects.forEach( project => {
-      if(filter === 'all' || filter === project.dataset.type){
+    projects.forEach((project) => {
+      if (filter === 'all' || filter === project.dataset.type) {
         project.classList.remove('invisible');
       } else {
         project.classList.add('invisible');
@@ -98,11 +119,5 @@ projectbtnContainer.addEventListener('click', event => {
     });
 
     projectContainer.classList.remove('anim-out');
-  }, 300) 
+  }, 300);
 });
-
-
-
-
-
-
